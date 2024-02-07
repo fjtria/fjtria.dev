8f781e1cac2787a6c240e7b22301eff56e72c044
FJ Tria <fjstria@gmail.com>
2024-02-06 22:21:04 -0800

    Fix

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/book-landing-page/",
+  base: "/fjstria.github.io",
   css: {
     modules: {
       localsConvention: "camelCase",
