2f57959aa1e6f7a9a079bdab8d6fe6306d154363
FJ Tria <fjstria@gmail.com>
2024-02-08 10:59:21 -0800

    Change font, update project images

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx';
 import './index.css';
 
 import "@fontsource/outfit";
-import "@fontsource/roboto";
+import "@fontsource/fira-sans";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
