df824fc49d7af134e3285c1dc569563174e35ee5
FJ Tria <fjstria@gmail.com>
2024-09-30 12:00:10 -0700

    Add skill images

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,5 +1,12 @@
 import '../styles/Skills.css'
-import image from '../assets/images/image.png'
+import reactImg from '../assets/images/react.png'
+import vueImg from '../assets/images/vue.png'
+import threeImg from '../assets/images/three.png'
+import nextImg from '../assets/images/next.png'
+import jsImg from '../assets/images/javascript.png'
+import tsImg from '../assets/images/typescript.png'
+import pythonImg from '../assets/images/python.png'
+import javaImg from '../assets/images/java.png'
 
 function Skills() {
   return(
@@ -8,38 +15,43 @@ function Skills() {
       <div className='skillList'>
 
         <div className='skillCard'>
-          <img src={image}></img>
-          <p>React</p>
+          <img src={reactImg} alt='React.js Logo'></img>
+          <p>React.js/React Native</p>
         </div>
 
         <div className='skillCard'>
-          <img src={image}></img>
-          <p>React</p>
+          <img src={vueImg}></img>
+          <p>Vue.js</p>
         </div>
 
         <div className='skillCard'>
-          <img src={image}></img>
-          <p>React</p>
+          <img src={threeImg}></img>
+          <p>Three.js</p>
         </div>
 
         <div className='skillCard'>
-          <img src={image}></img>
-          <p>React</p>
+          <img src={nextImg}></img>
+          <p>Next.js</p>
         </div>
 
         <div className='skillCard'>
-          <img src={image}></img>
-          <p>React</p>
+          <img src={jsImg}></img>
+          <p>JavaScript</p>
         </div>
 
         <div className='skillCard'>
-          <img src={image}></img>
-          <p>React</p>
+          <img src={tsImg}></img>
+          <p>TypeScript</p>
         </div>
 
         <div className='skillCard'>
-          <img src={image}></img>
-          <p>React</p>
+          <img src={pythonImg}></img>
+          <p>Python</p>
+        </div>
+
+        <div className='skillCard'>
+          <img src={javaImg}></img>
+          <p>Java</p>
         </div>
 
       </div>
