be61b489b752619beeb1bd6f11ed38abdedbdd4e
FJ Tria <fjstria@gmail.com>
2024-09-30 19:28:50 -0700

    Project section layout

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,9 +1,31 @@
 import '../styles/Projects.css'
+import defaultImg from '../assets/images/github.png'
 
 function Projects() {
   return(
    <div id='projects'>
     <h2>Check out my projects.</h2>
+    <div className='projectList'>
+      
+      <button className='projectCard'>
+        <img src={defaultImg}></img>
+        <h3>Pantry Pal</h3>
+        <p>An Android/iOS mobile app for sharing and discovering recipes.</p>
+      </button>
+
+      <button className='projectCard'>
+        <img src={defaultImg}></img>
+        <h3>Pantry Pal</h3>
+        <p>An Android/iOS mobile app for sharing and discovering recipes.</p>
+      </button>
+
+      <button className='projectCard'>
+        <img src={defaultImg}></img>
+        <h3>Pantry Pal</h3>
+        <p>An Android/iOS mobile app for sharing and discovering recipes.</p>
+      </button>
+    
+    </div>
    </div>
   );
 }
