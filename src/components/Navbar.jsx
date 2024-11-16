eced08f5c884e9b09d0bf7ea6e5437553fedcd35
FJ Tria <fjstria@gmail.com>
2024-11-15 17:34:50 -0800

    Adjust nav spacing

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import Resume from '../assets/documents/Tria_FJ_Resume.pdf'
 function Navbar() {
   return(
     <nav className='navbar'>
-      <div className='navbarRight'>
+      <div className='navbarLeft'>
         <h1>FJ TRIA</h1>
         {/*}
         <ul className='sections'>
@@ -15,7 +15,7 @@ function Navbar() {
         </ul>
         */}
       </div>
-      <ul className='navbarLeft'>
+      <ul className='navbarRight'>
         <li><a href={Resume} target='_blank'>RESUME</a></li>
         <li><a href="https://www.linkedin.com/in/fjstria/" target="_blank" rel="noopener noreferrer">LINKEDIN</a></li>
         <li><a href="#contact">CONTACT</a></li>
