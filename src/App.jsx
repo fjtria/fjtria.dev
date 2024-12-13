a9c5860200b07020411f28a13f44792b5792a734
FJ Tria <fjstria@gmail.com>
2024-12-12 21:26:49 -0800

    Fresh start

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,12 @@
-import './index.css'
-import Navbar from './components/Navbar'
-import About from './components/About'
-import Skills from './components/Skills'
-import Projects from './components/Projects'
-import Experience from './components/Experience'
-import Contact from './components/Contact'
-import Footer from './components/Footer'
+import './index.css';
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
 
   return (
-    <div>
+    <>
       <Navbar></Navbar>
-      <About></About>
-      <Projects></Projects>
-      <Experience></Experience>
-      {/*<Skills></Skills>*/}
-      <Contact></Contact>
-      <Footer></Footer>
-    </div>
+    </>
   );
 }
 
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import '../../index.css';
+
+export default function Navbar() {
+    return (
+        <nav>
+            <h1 className='title'>FJ TRIA</h1>
+
+            <p>Hi</p>
+        </nav>
+    );
+}
