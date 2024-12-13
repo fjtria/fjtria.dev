7faab773677849c4187b9ee975ca6e40c08e81f4
FJ Tria <fjstria@gmail.com>
2024-12-12 22:07:29 -0800

    Add new styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
+import React, { useState } from 'react';
 import './index.css';
 import Navbar from "./components/Navbar/Navbar";
 
 function App() {
+  const [isDark, setIsDark] = useState(false);
 
   return (
-    <>
+    <div data-theme={isDark ? "dark" : ""}>
       <Navbar></Navbar>
-    </>
+    </div>
   );
 }
 
