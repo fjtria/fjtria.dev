e3c9efe78de57247bb8fe83bfee353a67086fb33
FJ Tria <fjstria@gmail.com>
2025-01-11 20:17:30 -0800

    Update links

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -6,9 +6,9 @@ export default function Contact() {
         <section id='contact'>
             <h2 className='title'>Contact.</h2>
             <p className='subtitle'>Email:</p>
-            <a href='mailto:fjstria@gmail.com' target='_blank'>📧 fjstria@gmail.com</a>
+            <a href='mailto:fjtria.dev@gmail.com' target='_blank'>📧 fjtria.dev@gmail.com</a>
             <p className='subtitle'>LinkedIn:</p>
-            <a href='https://www.linkedin.com/in/fjstria/' target='_blank' rel='noopener noreferrer'>🤝 @fjstria</a>
+            <a href='https://www.linkedin.com/in/fjtria/' target='_blank' rel='noopener noreferrer'>🤝 @fjtria</a>
         </section>
     );
 }
