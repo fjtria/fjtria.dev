25753aecd58659bd9924afc4d520cab6b8a6c420
fj tria <fjtria.dev@gmail.com>
2025-02-18 20:43:58 -0800

    add footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import '../../index.css';
+import './Footer.css';
+
+export const Footer = () => {
+    return (
+        <footer>
+            <hr></hr>
+            <p>FJ Tria – 2025</p>
+        </footer>
+    );
+}
