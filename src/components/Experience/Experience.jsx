71b8d67ec7fef91ff98deecee9e0b30e8a5e1f58
fj tria <fjtria.dev@gmail.com>
2025-03-16 21:06:08 -0700

    update content

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -9,14 +9,6 @@ export const Experience = () => {
         <section id='experience'>
             <h2>Experience</h2>      
             <div className='exp-card'>
-                <img src={csufIcon} alt='California State University, Fullerton Logo' loading='lazy'></img>
-                <div>
-                    <h3>California State University, Fullerton</h3>
-                    <h3>Master's Degree</h3>
-                    <p className='exp-dates'>Expected December 2025</p>
-                </div>
-            </div>      
-            <div className='exp-card'>
                 <img src={spacelabIcon} alt='The Spacelab Nonprofit Logo' loading='lazy'></img>
                 <div>
                     <h3>The Spacelab Nonprofit</h3>
@@ -24,6 +16,14 @@ export const Experience = () => {
                     <p className='exp-dates'>October 2024 - Present</p>
                 </div>
             </div>
+            <div className='exp-card'>
+                <img src={csufIcon} alt='California State University, Fullerton Logo' loading='lazy'></img>
+                <div>
+                    <h3>California State University, Fullerton</h3>
+                    <h3>Master's Degree</h3>
+                    <p className='exp-dates'>Expected December 2025</p>
+                </div>
+            </div>      
             <div className='exp-card'>
             <img src={ucscIcon} alt='University of California, Santa Cruz Logo' loading='lazy'></img>
                 <div>
--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -12,50 +12,50 @@ import PantryPalPreview from '../../assets/images/Projects/pantrypal_preview.web
 export const Projects = () => {
     return (
         <section id='projects'>
-            <h2>Projects</h2> 
+            <h2>Projects</h2>  
 
             <div className='proj-card'>
-                <img src={DexfetchdPreview} loading='lazy'></img>
-                <div>
-                    <a href='https://fjtria.github.io/dexfetchd/' target='_blank' rel='noopener noreferrer'>Dexfetch'd ↗</a>
-                    <p className='proj-stack'>React.js, CSS, PokéAPI</p>
-                    <p className='proj-desc'>A web application for looking up basic data about Pokémon species, including types, abilities, and stats.</p>
-                </div>
-            </div>  
-
-            <div className='proj-card'>
-                <img src={SpacelabPreview} loading='lazy'></img>
+                <img src={SpacelabPreview} alt='Screenshot of the Spacelab Nonprofit website' loading='lazy'></img>
                 <div>
                     <a href='https://spacelab.space/' target='_blank' rel='noopener noreferrer'>The Spacelab Nonprofit ↗</a>
-                    <p className='proj-stack'>React.js, Sass, Node.js, CalTech API, AWS</p>
-                    <p className='proj-desc'>The Spacelab Nonprofit's website, for learning about the organization and discovering the latest findings in space.</p>
+                    <p className='proj-stack'>React, Sass, Node.js, AWS</p>
+                    <p className='proj-desc'>The Spacelab Nonprofit's website, describing the goals of the organization and discussing the latest space-related projects.</p>
                 </div>
             </div>  
 
             <div className='proj-card'>
-                <img src={ExoplanetariumPreview} loading='lazy'></img>
+                <img src={ExoplanetariumPreview} alt='Screenshot of the Exoplanetarium 3D web application' loading='lazy'></img>
                 <div>
                     <a href='https://spacelabdev.github.io/Exoplanetarium-3D/' target='_blank' rel='noopener noreferrer'>Exoplanetarium 3D ↗</a>
-                    <p className='proj-stack'>React.js, Node.js, Three.js, WebGL</p>
-                    <p className='proj-desc'>An in-progress, open-source, 3D web application for exploring and identifying exoplanets within our galaxy.</p>
+                    <p className='proj-stack'>React, Node.js, Three.js, WebGL</p>
+                    <p className='proj-desc'>An educational, interactive, 3D web application that renders and allows users to explore exoplanets within our galaxy.</p>
                 </div>
             </div>
+
+            <div className='proj-card'>
+                <img src={DexfetchdPreview} alt='Screenshot of the Dexfetchd website' loading='lazy'></img>
+                <div>
+                    <a href='https://fjtria.github.io/dexfetchd/' target='_blank' rel='noopener noreferrer'>Dexfetch'd ↗</a>
+                    <p className='proj-stack'>React, CSS, PokéAPI</p>
+                    <p className='proj-desc'>A website for interfacing with a database of Pokémon, allowing users to look up Pokémon species, including types, abilities, and stats.</p>
+                </div>
+            </div> 
             
             <div className='proj-card'>
-                <img src={ColorcodedPreview} loading='lazy'></img>
+                <img src={ColorcodedPreview} alt='Screenshot of the Colorcoded web application' loading='lazy'></img>
                 <div>
                     <a href='https://fjtria.github.io/colorcoded/' target='_blank' rel='noopener noreferrer'>Colorcoded ↗</a>
                     <p className='proj-stack'>JavaScript, HTML, CSS</p>
-                    <p className='proj-desc'>A color code conversion web application capable of converting between HEX, RGB, CMYK, and HSL formats.</p>
+                    <p className='proj-desc'>An all-in-one color code conversion tool capable of converting to and from the HEX, RGB, CMYK, and HSL color models.</p>
                 </div>
             </div>
 
             <div className='proj-card'>
-                <img src={PantryPalPreview} loading='lazy'></img>
+                <img src={PantryPalPreview} alt='Pantry Pal Logo' loading='lazy'></img>
                 <div>
                     <a href='https://github.com/fjtria/PantryPal' target='_blank' rel='noopener noreferrer'>Pantry Pal ↗</a>
                     <p className='proj-stack'>React Native, CSS, Firebase, Expo</p>
-                    <p className='proj-desc'>A mobile application that allows users to share and discover cooking recipes and generate grocery shopping lists.</p>
+                    <p className='proj-desc'>A mobile application allowing users to share and discover recipes and automatically generate grocery shopping lists.</p>
                 </div>
             </div>       
         </section>
