3e021989ed2edb7dca1c64d8c7d1aaa358697217
fj tria <fjtria.dev@gmail.com>
2025-04-12 00:14:37 -0700

    clean imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import { Route, Routes } from 'react-router'
 import { Navbar } from './components/Navbar/Navbar.jsx'
 import { Home } from './pages/Home/Home.jsx'
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router'
-import React from 'react'
 import App from './App.jsx'
 import './index.css'
 
